1692bcb9a8323e9616eebdf1f364705967140832
Riley <riley.childs@yahoo.com>
2024-04-14 13:28:19 -0400

    Reorganized code in create-post.js

--- views/create-post.js
+++ views/create-post.js
@@ -3,7 +3,7 @@ class Post {
     this.type = type;
     this.title = title;
     this.image = image;
-    this.content = content;
+    this.content = this.addPTags(content);
   }
 
   async add() {
@@ -21,84 +21,73 @@ class Post {
         }),
       });
 
-      if (response.status === 401) {
-        throw new Error("Failed to create post");
-      } else {
+      if (response.status === 201) {
         return response;
+      } else {
+        throw new Error("Failed to create post");
       }
-      
     } catch (error) {
       throw error;
     }
   }
 
-  success() {
+  addPTags(text) {
+    text = text.replace(/\n\n+/g, '</p><p class="post__paragraph">');
+    if (!text.startsWith('<p class="post__paragraph">')) {
+      text = '<p class="post__paragraph">' + text;
+    }
+    if (!text.endsWith("</p>")) {
+      text += "</p>";
+    }
+
+    return text;
+  }
+}
+
+class DomManipulation {
+  static success() {
     const tag = document.createElement("h1");
     tag.id = "success";
     tag.innerText = "Post successfully added";
-    document.getElementsByClassName("create-post__form")[0].reset();
-
-    if (document.getElementById("error")) {
-      document.getElementsByClassName("create-post")[0].append(tag);
-    }
+    document.querySelector(".create-post__form").reset();
 
     if (!document.getElementById("success")) {
-      document.getElementsByClassName("create-post")[0].append(tag);
+      document.querySelector(".create-post").append(tag);
     }
   }
 
-  error(message) {
+  static error(message) {
     const tag = document.createElement("h1");
     tag.id = "error";
     tag.innerText = message;
 
-    if (document.getElementById("success")) {
-      document.getElementsByClassName("create-post")[0].append(tag);
-    }
-
     if (!document.getElementById("error")) {
-      document.getElementsByClassName("create-post")[0].append(tag);
+      document.querySelector(".create-post").append(tag);
     }
   }
 }
 
-function addPTags(text) {
-  text = text.replace(/\n\n+/g, '</p><p class="post__paragraph">');
-  if (text.startsWith('<p class="post__paragraph">') === false) {
-    text = '<p class="post__paragraph">' + text;
+document.querySelector(".create-post__form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const newPost = {
+    type: document.querySelector(".create-post__type").value,
+    title: document.querySelector(".create-post__title").value,
+    image: document.querySelector(".create-post__img").value || null,
+    content: document.querySelector(".create-post__content").value,
+  };
+
+  const postReq = new Post(
+    newPost.type,
+    newPost.title,
+    newPost.image,
+    newPost.content
+  );
+
+  try {
+    await postReq.add();
+    DomManipulation.success();
+  } catch (error) {
+    DomManipulation.error(error.message);
   }
-  if (text.endsWith("</p>") === false) {
-    text += "</p>";
-  }
-
-  return text;
-}
-
-document
-  .getElementsByClassName("create-post__form")[0]
-  .addEventListener("submit", async (e) => {
-    e.preventDefault();
-
-    const newPost = {
-      type: document.getElementsByClassName("create-post__type")[0].value,
-      title: document.getElementsByClassName("create-post__title")[0].value,
-      image:
-        document.getElementsByClassName("create-post__img")[0].value || null,
-      content: document.getElementsByClassName("create-post__content")[0].value,
-    };
-
-    const postReq = new Post(
-      newPost.type,
-      newPost.title,
-      newPost.image,
-      addPTags(newPost.content)
-    );
-
-    try {
-      const sendPost = await postReq.add();
-
-      sendPost.status === 201 ? postReq.success() : Error;
-    } catch (error) {
-      postReq.error(error.message);
-    }
-  });
+});
