832bd75dd4de6356ff4f8b31fc9722da472d2a4e
Riley <riley.childs@yahoo.com>
2024-06-09 13:25:29 -0400

    Changed fetch route in manage-posts.js

--- views/manage-post.js
+++ views/manage-post.js
@@ -1,9 +1,9 @@
 // GENERATES AND FILLS DROPDOWN FOR BLOG TITLES
 (async ()=>{
     try {
-        const response = await fetch("/api/get-posts");
+        const response = await fetch("/api/get-post-titles");
         const data = await response.json();
-        console.log(data[0]);
+        console.log(data);
       } catch (error) {
         console.error('Error:', error);
       }
