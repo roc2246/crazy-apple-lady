5fcd97a943eed244558c4c4659d0acd5081035bf
Riley <riley.childs@yahoo.com>
2024-06-09 13:44:20 -0400

    Created functionality to select post to manage

--- views/manage-post.js
+++ views/manage-post.js
@@ -1,9 +1,18 @@
 // GENERATES AND FILLS DROPDOWN FOR BLOG TITLES
+function createOption(value){
+  const option = document.createElement("option")
+  option.value = value
+  option.innerText = value
+  return option
+}
+
 (async ()=>{
     try {
         const response = await fetch("/api/get-posts");
         const data = await response.json();
-        console.log(data[0]);
+        for(let x = 0; x < data.length; x++){
+          document.querySelector(".manage-post__select").append(createOption(data[x].title))
+        }
       } catch (error) {
         console.error('Error:', error);
       }
