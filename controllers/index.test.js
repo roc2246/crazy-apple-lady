994975e1186dd2ef362a5cba850e3f1519ea0f11
Riley <riley.childs@yahoo.com>
2024-07-07 18:21:47 -0400

    Created tests for retrieving post

--- controllers/index.test.js
+++ controllers/index.test.js
@@ -1,18 +1,19 @@
 import { describe, it, expect, vi } from "vitest";
-import { manageGetPostNames } from ".";
+import { manageGetPostNames, manageGetPost } from ".";
 
-function createMockResponse (){
+function createMockResponse() {
   const res = {};
-  res.status = vi.fn().mockReturnThis(); 
+  res.status = vi.fn().mockReturnThis();
   res.json = vi.fn();
+  res.send = vi.fn();
   return res;
-};
+}
 
-describe("Post Retrieval", () => {
+describe("Retrieving Post Names", () => {
   it("should return post names when available", async () => {
     // Arrange
     const req = {};
-    const res = createMockResponse(); 
+    const res = createMockResponse();
     const type = "plantyLife";
 
     // Act
@@ -25,11 +26,39 @@ describe("Post Retrieval", () => {
   it("should throw error if no post names are found", async () => {
     // Arrange
     const req = {};
-    const res = createMockResponse(); 
+    const res = createMockResponse();
     const type = "error";
 
     // Act
-    const post =  manageGetPostNames(req, res, type);
+    const post = manageGetPostNames(req, res, type);
+
+    // Assert
+    await expect(post).rejects.toThrowError();
+  });
+});
+
+describe("Retrieving Post", () => {
+  it("should return post", async () => {
+    // Arrange
+    const req = {};
+    const res = createMockResponse();
+    const id = 1;
+
+    // Act
+    await manageGetPost(req, res, id);
+
+    // Assert
+    expect(res.status).toHaveBeenCalledWith(200);
+  });
+
+  it("should throw error if no post names are found", async () => {
+    // Arrange
+    const req = {};
+    const res = createMockResponse();
+    const id = "error";
+
+    // Act
+    const post = manageGetPost(req, res, id);
 
     // Assert
     await expect(post).rejects.toThrowError();
