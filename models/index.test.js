a4e8afcf553b8320a6c40ed9c3e2d8571638e7e9
Riley <riley.childs@yahoo.com>
2024-10-06 18:10:26 -0400

    Refactored mongodb mock functions and variables to separate file

--- models/index.test.js
+++ models/index.test.js
@@ -8,131 +8,8 @@ import {
   updatePost,
 } from ".";
 import { addPTags } from "../utilities";
-import { Readable } from "stream"; // Import Readable for creating a stream
+import  *  as mongo from "../mocks/mongodb.js"
 
-let db = [];
-
-const mockPosts = [
-  {
-    _id: 21312313413,
-    id: 0,
-    type: "plantyLife",
-    title: "Plant Blog Post",
-    image: ["plant.jpg"],
-    content: "This is a post about plants.",
-  },
-  {
-    _id: 2131231341323542345,
-    id: 1,
-    type: "mushroomBlog",
-    title: "Mushroom Blog Post",
-    image: ["mushroom.jpg"],
-    content: "This is a post about mushrooms.",
-  },
-];
-
-// Create a mock for the connectToDB function
-const mockConnectToDB = vi.fn();
-
-// Create a mock for the MongoDB collection
-const mockFindOne = vi.fn();
-const mockInsertOne = vi.fn((post) => (db = [...db, post]));
-const mockFindOneAndUpdate = vi.fn(({ id }, update) => {
-  update["$set"].id = id;
-  update = update["$set"];
-
-  db[id].id = id;
-  db[id].type = update.type || db[id].type;
-  db[id].title = update.title || db[id].title;
-  db[id].image = update.image || db[id].image;
-  db[id].content = update.content || db[id].content;
-});
-const mockFindOneAndDelete = vi.fn((id) => db.splice(id));
-
-const mockAggregate = vi.fn((pipeline) => {
-  let results = [];
-
-  // $match operator
-  const match = pipeline.find((array) => array.hasOwnProperty("$match"));
-  if (match) {
-    const matchArgs = match.$match;
-    const key = Object.keys(matchArgs)[0];
-    const value = matchArgs[key];
-
-    // Filter posts in a single pass
-    results = mockPosts.reduce((acc, post) => {
-      if (post[key] === value) acc.push(post);
-      return acc;
-    }, []);
-  }
-
-  // $project operator
-  const project = pipeline.find((array) => array.hasOwnProperty("$project"));
-  if (project) {
-    const projectArgs = project.$project;
-    const keys = Object.keys(projectArgs);
-
-    let keyCount = 0;
-    let resultsCount = 0;
-    while (keyCount < keys.length) {
-      if (resultsCount === results.length) {
-        resultsCount = 0;
-        keyCount++;
-      }
-      if (
-        results[resultsCount].hasOwnProperty(keys[keyCount]) &&
-        projectArgs[keys[keyCount]] === 0
-      ) {
-        delete results[resultsCount][keys[keyCount]];
-        resultsCount++;
-      } else {
-        resultsCount = 0;
-        keyCount++;
-      }
-    }
-    if (keyCount >= keys.length) keyCount = 0;
-
-    let newResults = [];
-    results.forEach(obj  => newResults.push({}))
-
-    while (keyCount < keys.length) {
-      if (resultsCount === results.length) {
-        resultsCount = 0;
-        keyCount++;
-      }
-      if (
-        results[resultsCount].hasOwnProperty(keys[keyCount]) &&
-        projectArgs[keys[keyCount]] === 1
-      ) {
-        newResults[resultsCount][keys[keyCount]] = results[resultsCount][keys[keyCount]]
-        resultsCount++;
-      } else {
-        resultsCount = 0;
-        keyCount++;
-      }
-    }
-    if (!newResults.some(obj => Object.keys(obj).length === 0)) results = newResults;
-  }
-
-  // Return an object with a stream method
-  return {
-    stream: () => Readable.from(results),
-  };
-});
-
-const mockCollection = {
-  findOne: mockFindOne,
-  insertOne: mockInsertOne,
-  findOneAndUpdate: mockFindOneAndUpdate,
-  findOneAndDelete: mockFindOneAndDelete,
-  aggregate: mockAggregate,
-};
-
-// Create a mock for the database instance
-const mockDb = { collection: vi.fn(() => mockCollection) };
-
-// Create a mock return value for connectToDB
-mockConnectToDB.mockResolvedValue({ db: mockDb });
 
 describe("newPost", () => {
   const post = {
@@ -143,9 +20,9 @@ describe("newPost", () => {
     content: "TEST",
   };
   it("should add new post", async () => {
-    const priorLength = db.length;
-    await newPost(post, mockConnectToDB);
-    expect(db.length).toBeGreaterThan(priorLength);
+    const priorLength = mongo.db.length;
+    await newPost(post, mongo.mockConnectToDB);
+    expect(mongo.db.length).toBeGreaterThan(priorLength);
   });
 });
 
@@ -158,19 +35,19 @@ describe("updatePost", () => {
       image: ["test.jpg"],
       content: addPTags("TEST1212"),
     };
-    await updatePost(updatedPost, mockConnectToDB);
-    const newPost = db[0];
+    await updatePost(updatedPost, mongo.mockConnectToDB);
+    const newPost = mongo.db[0];
     expect(newPost).toEqual(updatedPost);
   });
 });
 
 describe("deletePost", () => {
   it("should delete post", async () => {
-    await deletePost(0, mockConnectToDB);
-    expect(db.length).toBe(0);
+    await deletePost(0, mongo.mockConnectToDB);
+    expect(mongo.db.length).toBe(0);
   });
   it("should throw error", async () => {
-    const result = await deletePost(100, mockConnectToDB);
+    const result = await deletePost(100, mongo.mockConnectToDB);
     expect(result).toThrowError;
   });
 });
@@ -180,7 +57,7 @@ describe("retrieving post names", () => {
     const match = { type: "plantyLife" };
     const project = { _id: 0, id: 1, title: 1 };
 
-    const results = await postRetrieval(match, project, mockConnectToDB);
+    const results = await postRetrieval(match, project, mongo.mockConnectToDB);
     // Adjust the expected result to match the actual structure you expect
     const expectedResults = [
       {
@@ -195,7 +72,7 @@ describe("retrieving post names", () => {
     const match = { type: "mushroomBlog" };
     const project = { _id: 0, id: 1, title: 1 };
 
-    const results = await postRetrieval(match, project, mockConnectToDB);
+    const results = await postRetrieval(match, project, mongo.mockConnectToDB);
     // Adjust the expected result to match the actual structure you expect
     const expectedResults = [
       {
@@ -213,7 +90,7 @@ describe("Get full post", () => {
     const match = { id: 0 };
     const project = { _id: 0 };
 
-    const results = await postRetrieval(match, project, mockConnectToDB);
+    const results = await postRetrieval(match, project, mongo.mockConnectToDB);
     const expectedResults = [{
       id: 0,
       type: 'plantyLife',
