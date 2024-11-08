ae2972b95cc187312809a975268b6e671f31e9eb
Riley <riley.childs@yahoo.com>
2024-11-08 14:07:08 -0500

    Creted test file for utilities

---
+++ utilities/index.test.js
@@ -0,0 +1,12 @@
+import {
+    vi,
+    describe,
+    it,
+    expect,
+    beforeEach,
+    beforeAll,
+    afterAll,
+  } from "vitest";
+import * as utilities from "../utilities/index.js";
+const fs = require("fs");
+const path = require("path");
