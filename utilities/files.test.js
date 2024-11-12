a6a8fd174c380eabaf0ba09cf869910580df8fcf
Riley <riley.childs@yahoo.com>
2024-11-12 11:51:18 -0500

    Updated test for uploading images not in uploads dir

--- utilities/files.test.js
+++ utilities/files.test.js
@@ -12,34 +12,37 @@ import * as formidable from "../mocks/formidable.js";
 const fs = require("fs").promises;
 const path = require("path");
 
-
 const tag = "tstBlog";
 
 beforeAll(() => {
-  formidable.newDirectory("mockDir")
+  formidable.newDirectory("mockDir");
   formidable.newDirectory(formidable.mockPath.temp);
   formidable.newDirectory(formidable.mockPath.server);
 });
 afterAll(() => {
-  formidable.deleteDirectory("mockDir")
+  formidable.deleteDirectory("mockDir");
 });
 
 describe("File Mangement", () => {
-  it("should create initial files to upload", async()=>{
-    formidable.createFiles(formidable.mockImgs.newPost, formidable.mockPath.temp);
+  it("should create initial files to upload", async () => {
+    formidable.createFiles(
+      formidable.mockImgs.newPost,
+      formidable.mockPath.temp
+    );
 
-    const mockTempFiles = await fs.readdir(formidable.mockPath.temp)
-    const createdImgNames = formidable.mockImgs.newPost
+    const mockTempFiles = await fs.readdir(formidable.mockPath.temp);
+    const createdImgNames = formidable.mockImgs.newPost;
 
-    expect(mockTempFiles.length).toBe(createdImgNames.length)
-  })
+    expect(mockTempFiles.length).toBe(createdImgNames.length);
+    createdImgNames.forEach((name) => expect(mockTempFiles).toContain(name));
+  });
   it("should upload files", async () => {
     const files2Upload = formidable.setImgsToUpload(formidable.mockPath.temp);
     await utilities.uploadFiles(files2Upload, formidable.mockPath.server, tag);
 
-    const mockTempFiles = await fs.readdir(formidable.mockPath.temp)
+    const mockTempFiles = await fs.readdir(formidable.mockPath.temp);
     const mockUploads = await fs.readdir(formidable.mockPath.server);
-    const createdImgNames = formidable.mockImgs.newPost
+    const createdImgNames = formidable.mockImgs.newPost;
 
     expect(mockUploads.length).toBe(createdImgNames.length);
     expect(mockTempFiles.length).toBe(0);
@@ -50,13 +53,29 @@ describe("File Mangement", () => {
 
   it("should throw an error uploading files", async () => {
     const initImgs = "TERST";
-    const results = utilities.uploadFiles(initImgs, formidable.mockPath.server, tag);
+    const results = utilities.uploadFiles(
+      initImgs,
+      formidable.mockPath.server,
+      tag
+    );
 
     await expect(results).rejects.toThrowError();
   });
 
+  it("should create images for the following tests", async () => {
+    formidable.createFiles(
+      formidable.mockImgs.updatePost,
+      formidable.mockPath.temp
+    );
+    const mockTempFiles = await fs.readdir(formidable.mockPath.temp);
+    const createdImgNames = formidable.mockImgs.updatePost;
+
+    expect(mockTempFiles.length).toBe(createdImgNames.length);
+    createdImgNames.forEach((name) => expect(mockTempFiles).toContain(name));
+  });
+
   // it(`should update images of ${tag}`, async () => {
-  //   formidable.createFiles(formidable.mockImgs.updatePost, formidable.mockPath.temp);
+  //
   //   const initImgs = formidable.setImgsToUpload(formidable.mockPath.temp);
   //   const localImgs = fs.readdirSync(formidable.mockPath.temp);
 
@@ -84,7 +103,6 @@ describe("File Mangement", () => {
   //   await expect(results).rejects.toThrowError();
   // });
 
-
   // it(`should remove all ${tag} files`, async () => {
   //   const uploadedImgsBefore = fs.readdirSync(formidable.mockPath.server);
   //   await utilities.removeFiles(uploadedImgsBefore, formidable.mockPath.server, tag);
@@ -98,5 +116,4 @@ describe("File Mangement", () => {
 
   //   await expect(results).rejects.toThrowError();
   // });
-
 });
